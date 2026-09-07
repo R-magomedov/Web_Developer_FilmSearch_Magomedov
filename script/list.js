38855e266f5c76631a1b7911d0177343b702e2ba
R-magomedov <radjab.magomedov1995@gmail.com>
2026-09-07 19:38:23 +0300

    feat: добавил фильтрацию фильмов через API

--- script/list.js
+++ script/list.js
@@ -1,70 +1,121 @@
-// type=movie
-// rating.kp=7-10
-// releaseYears.end=2020-2026
-// genres.name=
-// countries.name=
+// type=tv-series
 
 import { fetchMovies } from "./api.js";
 import { searchForm, showMovies } from "./render-cards.js";
 import { initSlider } from "./slider.js";
 
-const API_LIST = 'https://api.poiskkino.dev/v1.5/movie?notNullFields=name,rating.kp,poster.url&limit=45&withCount=false';
-
-let currentPage = 1;
+const API_LIST = 'https://api.poiskkino.dev/v1.5/movie?type=movie&notNullFields=name,rating.kp,poster.url&limit=45';
 const itemsPerPage = 9;
+
 let allMovies = [];
+let currentPage = 1;
+let currentGenre = '';
+let currentCountry = '';
+let currentYear = '';
+let currentRating = '';
 
+const genre = document.getElementById('genre')
+genre.addEventListener('change', () => {
+    currentGenre = genre.value;
+    toggleActive(genre);
+    loadMovies();
+});
 
-async function initCatalog() {
+const country = document.getElementById('country');
+country.addEventListener('change', () => {
+    currentCountry = country.value;
+    toggleActive(country);
+    loadMovies();
+});
+
+const year = document.getElementById('year');
+year.addEventListener('change', () => {
+    currentYear = year.value;
+    toggleActive(year);
+    loadMovies();
+});
+
+const rating = document.getElementById('rating');
+rating.addEventListener('change', () => {
+    currentRating = rating.value;
+    toggleActive(rating);
+    loadMovies();
+});
+
+function toggleActive(select) {
+    select.classList.toggle('active', !!select.value)
+}
+
+function buildApi() {
+    let url = API_LIST;
+    if (currentGenre) {
+        url = `${url}&genres.name=${currentGenre}`;
+    }
+    if (currentRating) {
+        url = `${url}&rating.kp=${currentRating}`;
+    }
+    if (currentYear) {
+        url = `${url}&year=${currentYear}`;
+    }
+    if (currentCountry) {
+        url = `${url}&countries.name=${currentCountry}`;
+    }
+    return url;
+}
+
+async function loadMovies() {
     try {
-        const data = await fetchMovies(API_LIST);
+        const url = buildApi();
+        const data = await fetchMovies(url);
         allMovies = data.docs || [];
         renderPage(1);
-        initSlider(document.querySelector('.catalog__slider'), 15);
-        searchForm((data) => {
-            document.querySelector('#ul').scrollIntoView({ behavior: 'smooth' });
-            showMovies(data.docs, 'ul')
-        });
-
-
     } catch (error) {
         console.error('Ошибка сети:', error);
     }
+}
 
-    function renderPage(pageNum) {
-        currentPage = pageNum;
+function renderPage(pageNum) {
+    currentPage = pageNum;
 
-        const start = (pageNum - 1) * itemsPerPage;
-        const end = start + itemsPerPage;
-        const pageMovies = allMovies.slice(start, end);
+    const start = (pageNum - 1) * itemsPerPage;
+    const end = start + itemsPerPage;
+    const pageMovies = allMovies.slice(start, end);
 
-        showMovies(pageMovies, 'ul');
-        updatePageNumbers();
+    showMovies(pageMovies, 'ul');
+    updatePageNumbers();
 
-    };
+};
 
-    function updatePageNumbers() {
-        const container = document.querySelector('.pages');
-        container.innerHTML = '';
+function updatePageNumbers() {
+    const container = document.querySelector('.pages');
+    const maxPages  = Math.ceil(allMovies.length / itemsPerPage);
+    container.innerHTML = '';
 
 
-        for (let i = 1; i <= 5; i++) {
-            const btn = document.createElement('span');
-            btn.textContent = i;
-            btn.className = 'page';
+    for (let i = 1; i <= maxPages; i++) {
+        const btn = document.createElement('span');
+        btn.textContent = i;
+        btn.className = 'page';
 
-            if (i === currentPage) {
-                btn.classList.add('page--active');
-            }  
-
-            btn.addEventListener('click', () => {
-                renderPage(i);
-            })
-
-            container.appendChild(btn);
+        if (i === currentPage) {
+            btn.classList.add('page--active');
         }
-    };
 
+        btn.addEventListener('click', () => {
+            renderPage(i);
+        })
+
+        container.appendChild(btn);
+    }
+};
+
+async function initCatalog() {
+    await loadMovies();
+    initSlider(document.querySelector('.catalog__slider'), 15);
+    searchForm((data) => {
+        document.querySelector('#ul').scrollIntoView({ behavior: 'smooth' });
+        showMovies(data.docs, 'ul')
+    });
 }
 
 initCatalog();
